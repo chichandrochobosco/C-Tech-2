e35b42aefaea844d58448a07294afbaf3b4ee259
EloyTejero <tejero.eloyet36@gmail.com>
2023-08-24 19:45:53 -0300

    Add Enum TipoTransaccion, clase Transaccion atributos

--- Code/Ruta12CarlitosTech/src/core/Transaccion.java
+++ Code/Ruta12CarlitosTech/src/core/Transaccion.java
@@ -1,6 +1,30 @@
 
 package core;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Transaccion {
+    static int idGlobal;
+    int id;
+    TipoTransaccion tipo;
+    Camion camion;
+    Conductor conductor;
+    ArrayList<Palet> mercaderia;
+    LocalDate fecha = LocalDate.now();
+    String origen;
+    String destino;
+
+    public Transaccion(TipoTransaccion tipo, Camion camion, Conductor conductor, ArrayList<Palet> mercaderia, String origen, String destino) {
+        this.id = idGlobal;
+        this.tipo = tipo;
+        this.camion = camion;
+        this.conductor = conductor;
+        this.mercaderia = mercaderia;
+        this.origen = origen;
+        this.destino = destino;
+        idGlobal++;
+    }
+    
     
 }
