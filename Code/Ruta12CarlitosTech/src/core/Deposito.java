71ec5cab8a1cabb5fbc918cead108aa2eb1a7945
EloyTejero <tejero.eloyet36@gmail.com>
2023-08-24 20:38:57 -0300

    Add Comentarios

--- Code/Ruta12CarlitosTech/src/core/Deposito.java
+++ Code/Ruta12CarlitosTech/src/core/Deposito.java
@@ -16,27 +16,28 @@ public class Deposito {
     
     //Muestra la lista de la mercaderia en todos los palets ocupados
     public void verStock(){
-        
+        //eloy
     }
     
     //Muestra las unidades de palet que se encuentran vacias (Su pasillo, estanteria y numero de palet. Todo por el indice interno)
-    public void verEspaciosVacios(){
-        
+    public int[] verEspaciosVacios(){
+        //eloy
     }
     
     //Agrega una transaccion a la lista y ejecuta el metodo de la transaccion necesario
     public void realizarTransaccion(){
-        
+        //preguntar que transaccion hacer y en que coordenadas
+        //licha
     }
     
     //Borrar los palets indicados en el egreso
     public void egreso(/*que palets borrar*/){
-        
+        //licha
     }
     
     //Asigna a un palet indicado los valores de ingreso
     public void ingreso(){
-        
+        //lolo
     }
     
     //muestra todas las transacciones
